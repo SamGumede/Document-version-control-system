b22d7c27120f81190d4e161ddd46adc0ac22af50
Sam Gumede <samgumede5@gmail.com>
2018-11-18 01:34:35 +0200

    Update files.sql

---
+++ files.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.7
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 16, 2018 at 11:17 PM
+-- Server version: 10.1.30-MariaDB
+-- PHP Version: 7.2.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `files`
+--
+CREATE DATABASE IF NOT EXISTS `files` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `files`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `addfiles`
+--
+
+DROP TABLE IF EXISTS `addfiles`;
+CREATE TABLE `addfiles` (
+  `id` int(11) NOT NULL,
+  `keyword` varchar(255) NOT NULL,
+  `author` varchar(255) NOT NULL,
+  `filename` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `addfiles`
+--
+
+INSERT INTO `addfiles` (`id`, `keyword`, `author`, `filename`) VALUES
+(1, 'Computer science', 'Sam Gumede', 'class work.txt');
+(2 , 'Business Management','Sydney Baloyi','logistics presentation.pdf');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `addfiles`
+--
+ALTER TABLE `addfiles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `addfiles`
+--
+ALTER TABLE `addfiles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
